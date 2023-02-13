f08a8ff43c08bfa0d8d3a0c92030317b2bae6be5
Shekhar Singh <shekhar.singh@xpo.com>
2023-02-13 18:23:41 +0530

    Added Test Case

--- RockPaperScissors.Test/HandSignTest.cs
+++ RockPaperScissors.Test/HandSignTest.cs
@@ -6,37 +6,35 @@ using RockPaperScissors.Model;
 namespace RockPaperScissors.Test
 {
 
-    public class HandSignTest2
+    public class HandSignTest
     {
         private HandSign _exeHandSignResult;
 
-        readonly IPrintUtil iprintUtil;
-        readonly IHandSign ihandSign = new HandSign();
+        private readonly IHandSign ihandSign = new HandSign();
 
-        public HandSignTest2() {
-            iprintUtil = new PrintUtil(ihandSign);
+        public HandSignTest() {
         }
 
-        ///// <summary>
-        ///// Handlers the with valid input positive test.
-        ///// </summary>
-        //[Fact(DisplayName = "HandSignWithMapStringToMove : Positive Scenario: Valid input")]
-        //public void HandSignWithMapStringToMove()
-        //{
-        //    GetValidHandSign();
-        //    CheckMapStringToMoveExpectedValue();
-        //}
-        //private void GetValidHandSign()
-        //{
-        //    string input = "R";
-        //    _exeHandSignResult = ihandSign.MapStringToMove(input);
-        //}
-        ///// <summary>
-        ///// To check the expected value -
-        ///// </summary>
-        //private void CheckMapStringToMoveExpectedValue()
-        //{
-        //    Assert.NotNull(_exeHandSignResult);
-        //}
+        /// <summary>
+        /// Handlers the with valid input positive test.
+        /// </summary>
+        [Fact(DisplayName = "HandSignWithMapStringToMove : Positive Scenario: Valid input")]
+        public void HandSignWithMapStringToMove()
+        {
+            GetValidHandSign();
+            CheckMapStringToMoveExpectedValue();
+        }
+        private void GetValidHandSign()
+        {
+            string input = "R";
+            _exeHandSignResult = ihandSign.MapStringToMove(input);
+        }
+        /// <summary>
+        /// To check the expected value -
+        /// </summary>
+        private void CheckMapStringToMoveExpectedValue()
+        {
+            Assert.NotNull(_exeHandSignResult);
+        }
     }
 }
